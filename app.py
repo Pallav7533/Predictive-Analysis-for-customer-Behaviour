1318d9bbc5fe16e07736fd2f76d55e5d5a531f7f
pallav chavda <pallavdrj7533@gmail.com>
2024-04-12 13:43:35 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split  # Correct import
